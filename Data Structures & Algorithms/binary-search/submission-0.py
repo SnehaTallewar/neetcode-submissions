e1ea328f03582d17d2eada82a6a2508ba4eff943
Sneha Tallewar <126948395+SnehaTallewar@users.noreply.github.com>
2026-05-19 18:16:54 +0530

    Bulk sync: 13 submissions

---
+++ Data Structures & Algorithms/binary-search/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        min_ele=0
+        max_ele=len(nums)-1
+        for i in range(len(nums)):
+            if target== nums[min_ele]:
+                return i
+            elif target == nums[max_ele]:
+                return max_ele
+            mid = (min_ele+max_ele)/2
+            if target==mid:
+                return mid
+            else:
+                min_ele+=1
+                max_ele-=1
+        return -1
+            
+        
